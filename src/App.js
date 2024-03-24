4720c3beaf317e5ce4ae71826888581dcabeb223
byunkihyuk <rlgur109@naver.com>
2024-03-25 00:12:28 +0900

    Add: "페이지 라우팅 추가"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Main from "./pages/main";
+import SignIn from "./pages/user/SignIn";
+import SignUp from "./pages/user/SignUp";
+import Mypage from "./pages/user/Mypage";
+import { useState } from "react";
+import { AppContext } from "./AppContext";
+import ChattingRooms from "./pages/chatting/ChattingRooms";
+import AllGeneralTransaction from "./pages/general/AllGeneralTransaction";
+import AllAuctionTransaction from "./pages/auction/AllAuctionTransaction";
+import GeneralTransaction from "./pages/general/GeneralTransaction";
+import AuctionTransaction from "./pages/auction/AuctionTransaction";
+import SearchPage from "./pages/SearchPage";
+import Loading from "./components/Loading";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loginToken, setLoginToken] = useState("");
+    return (
+        <AppContext.Provider value={{ loginToken, setLoginToken }}>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/main" element={<Main />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/general" element={<AllGeneralTransaction />} />
+                    <Route path="/general/:generalTransactionId" element={<GeneralTransaction />} />
+                    <Route path="/auction" element={<AllAuctionTransaction />} />
+                    <Route path="/auction/:auctionTransactionId" element={<AuctionTransaction />} />
+                    <Route path="/mypage" element={<Mypage />} />
+                    <Route path="/search" element={<SearchPage />} />
+                    <Route path="/chat" element={<ChattingRooms />} />
+                    <Route path="/load" element={<Loading />} />
+                </Routes>
+            </div>
+        </AppContext.Provider>
+    );
 }
 
 export default App;
