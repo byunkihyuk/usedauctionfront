27a9978ceb839df00c1901c3bf14257f1548b550
byunkihyuk <rlgur109@naver.com>
2024-05-17 19:08:10 +0900

    Update: "setupProxy.js 설정 수정"
    오타 제거

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: process.env.REACT_APP_CLIENT_PROXY2,
+            target: process.env.REACT_APP_CLIENT_PROXY,
             changeOrigin: true,
         })
     );
