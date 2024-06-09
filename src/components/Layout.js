9670a80be88f74a8fb275a9494789cdcf7208f02
buynkihyuk <rlgur109@naver.com>
2024-06-10 00:46:24 +0900

    Update: "Layout 수정"

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ function Layouts(props) {
     return (
         <section className="relative  w-full h-full scroll-auto flex flex-col items-center justify-center">
             <Header />
-            <section className="top-0 py-16 h-full min-h-[600px] lg:max-w-[1000px] sm:w-auto  ">
+            <section className="top-0 py-16 px-2 h-full min-h-[600px] lg:w-auto sm:w-auto w-auto md:w-auto">
                 {props.children}
             </section>
             <Footer />
