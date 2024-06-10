4cf5ca0ea8e6c8749cc6a86d404297eec39e58df
buynkihyuk <rlgur109@naver.com>
2024-06-11 04:01:01 +0900

    Update: "ProductCard 컴포넌트 날짜 표현 수정, 중고 글 경매 글 표시 옵션 추가"
    중고 글
    가격, 결제 방법, 거래 방법, 조회수, 등록 날짜 표시
    
    경매 글
    시작가, 최고가, 경매 기간, 조회수, 등록 날짜 표시

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
-import noimage from "../images/search.png";
+import noimage from "../images/noimage.png";
 
 function ProductCard(props) {
     const date = new Date(props.info.createdAt);
     const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
     const transactionState =
         props.info.transactionState == null || props.info.transactionState === ""
             ? "판매중"
             : props.info.transactionState;
 
+    function dateFormat(date) {
+        let now = new Date(date);
+        let nowYear = now.getYear() >= 100 ? now.getYear() - 100 : now.getYear();
+        let nowMonth = now.getMonth() + 1;
+        let nowDay = now.getDate();
+        let nowHours = now.getHours();
+        let nowMinutes = now.getMinutes();
+
+        return nowYear + "-" + nowMonth + "-" + nowDay + " " + nowHours + ":" + nowMinutes;
+    }
+
     return (
         <div className="w-auto h-auto">
-            <div className="flex flex-col w-48 h-80 border-[1px] hover:border-purple-500  rounded-lg   overflow-hidden">
-                <Link to={props.url}>
-                    <div className=" w-48 h-48 flex items-center justify-center overflow-hidden cursor-pointer">
+            <Link to={props.url}>
+                <div className="flex flex-col w-48 h-80 border-[1px] hover:border-purple-500  rounded-lg   overflow-hidden cursor-pointer">
+                    <div className="relative w-48 h-48 flex items-center justify-center overflow-hidden ">
+                        {props.type !== "general" &&
+                            transactionState === "판매중" &&
+                            new Date(props.info.finishedAt) < new Date() && (
+                                <div className="absolute text-2xl font-bold text-purple-500 w-48 h-48 flex items-center justify-center">
+                                    <div className="absolute w-48 h-48 bg-black opacity-60"></div>
+                                    <div className="absolute">{"입찰 종료"}</div>
+                                </div>
+                            )}
                         {transactionState !== "판매중" && (
                             <div className="absolute text-2xl font-bold text-purple-500 w-48 h-48 flex items-center justify-center">
                                 <div className="absolute w-48 h-48 bg-black opacity-60"></div>
@@ -29,27 +48,57 @@ function ProductCard(props) {
                             className={`w-48 h-48 object-cover `}
                         ></img>
                     </div>
-                </Link>
-                <div className="h-32 flex flex-col justify-between p-2">
-                    <div className="flex items-center justify-start">
-                        <Link to={props.url}>
+                    <div className="h-32 flex flex-col justify-between p-2">
+                        <div className="flex items-center justify-start">
                             <b>{props.info.title ? props.info.title : "제목이 없습니다"}</b>
-                        </Link>
-                    </div>
-                    <div className="flex items-center justify-start text-sm">
-                        가격&nbsp;{Number(props.info.price).toLocaleString("ko-KR")}원
-                    </div>
-                    <div className="flex items-center justify-start text-sm">
-                        {props.type ? props.type : "없음"}
-                    </div>
-                    <div className="flex items-center justify-between text-sm">
-                        <div className="">최고가</div>
-                        <div>
-                            {year}. {month}. {day}.
+                        </div>
+                        <div className="flex items-center justify-start text-sm">
+                            {props.type === "general" ? (
+                                <div>
+                                    가격&nbsp;{Number(props.info.price).toLocaleString("ko-KR")}원
+                                </div>
+                            ) : (
+                                <div className="w-full flex justify-between">
+                                    <div
+                                        title={Number(props.info.price).toLocaleString() + "원"}
+                                        className="w-20 overflow-hidden text-ellipsis text-nowrap text-left"
+                                    >
+                                        시작 {Number(props.info.price).toLocaleString()}원
+                                    </div>
+                                    <div
+                                        title={
+                                            Number(props.info.highestBid).toLocaleString() + "원"
+                                        }
+                                        className="w-20 overflow-hidden text-ellipsis text-nowrap text-right"
+                                    >
+                                        최고 {Number(props.info.highestBid).toLocaleString()}원
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        <div className="flex items-center justify-start text-sm">
+                            {props.type == "general" ? (
+                                <div className="w-full flex justify-between">
+                                    <div>거래 {props.info.transactionMode}</div>
+                                    <div>결제 {props.info.payment}</div>
+                                </div>
+                            ) : (
+                                <div className="w-full flex justify-between text-xs">
+                                    <div>{dateFormat(props.info.startedAt)}</div>
+                                    <div>~</div>
+                                    <div>{dateFormat(props.info.finishedAt)}</div>
+                                </div>
+                            )}
+                        </div>
+                        <div className="flex items-center justify-between text-sm">
+                            <div>조회수 {props.info.viewCount}</div>
+                            <div>
+                                {year}. {month}. {day}.
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 }
