91acd21d2eccdf121e35e77da6cc544b318fbe68
buynkihyuk <rlgur109@naver.com>
2024-07-14 23:38:53 +0900

    Update: "Layout 너비 계산 수정"

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,17 +17,30 @@ function Layouts(props) {
 
     const [platForm, setPlatForm] = useState(null);
 
+    const getWindowDimensions = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return { width, height };
+    };
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
     useEffect(() => {
-        if (window.innerWidth >= 1024) {
+        const handleResize = () => {
+            setWindowDimensions(getWindowDimensions());
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (windowDimensions.width >= 1024) {
             setPlatForm(true);
         } else {
             setPlatForm(false);
         }
-    }, []);
+    }, [windowDimensions]);
 
     return (
         <section className="relative  w-full h-full scroll-auto flex flex-col items-center justify-center">
-            {/* {platForm ? <Header /> : <Header2 />} */}
             {platForm ? <Header /> : <Header2 />}
             <section className="top-0 py-16 h-full min-h-[600px] flex justify-center w-auto pc:min-w-[1000px] mobile:min-w-96">
                 {props.children}
